519447e440ee9b1ee1bb907739d7e9b150203f18
goldcider <joe@coldridge.com>
2026-05-19 22:41:13 +0100

    Mass Delete

--- MS-50G+ Expansion Delay & Reverb.bat
+++
@@ -1,4 +0,0 @@
-curl -H "User-Agent: HandyGuitarLab/1.0" -o PhaseDly.zip https://zoom.co.jp
-curl -H "User-Agent: HandyGuitarLab/1.0" -o SlowAtkD.zip https://zoom.co.jp
-curl -H "User-Agent: HandyGuitarLab/1.0" -o AirReverb.zip https://zoom.co.jp
-curl -H "User-Agent: HandyGuitarLab/1.0" -o IceDelay.zip https://zoom.co.jp
--- MS-70CDR+ SFX & Utilities.bat
+++
@@ -1,6 +0,0 @@
-curl -H "User-Agent: HandyGuitarLab/1.0" -o BitCrush.zip https://zoom.co.jp
-curl -H "User-Agent: HandyGuitarLab/1.0" -o CombFltr.zip https://zoom.co.jp
-curl -H "User-Agent: HandyGuitarLab/1.0" -o Bomber.zip https://zoom.co.jp
-curl -H "User-Agent: HandyGuitarLab/1.0" -o MonoSynth.zip https://zoom.co.jp
-curl -H "User-Agent: HandyGuitarLab/1.0" -o Z-Organ.zip https://zoom.co.jp
-curl -H "User-Agent: HandyGuitarLab/1.0" -o AutoPanSFX.zip https://zoom.co.jp
